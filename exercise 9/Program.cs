c3258c97767e9738f09361221b895f039c5cbf97
Tito-Arroyo <tito.arroyo@s.capregboces.org>
2024-03-05 13:20:37 -0500

    ex 9

---
+++ exercise 9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Transactions;
+
+namespace exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your width");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter your height");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int a = 1; a <= height; a++)
+            {
+                for (int b = 1; b <= width; b++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
