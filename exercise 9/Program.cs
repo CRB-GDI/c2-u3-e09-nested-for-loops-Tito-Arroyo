1b3eeb5316b86202d84110af93d613df97759201
Tito-Arroyo <tito.arroyo@s.capregboces.org>
2024-03-06 13:05:49 -0500

    Update Program.cs

--- exercise 9/Program.cs
+++ exercise 9/Program.cs
@@ -17,7 +17,16 @@ namespace exercise_9
                 {
                     Console.Write("*");
                 }
-
+                Console.WriteLine();
+            }
+            Console.WriteLine("max number of * in a row");
+            int astPeRrow = int.Parse(Console.ReadLine());
+            for (int a = 1; a <= astPeRrow; a++)
+            {
+                for (int b = 1; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
                 Console.WriteLine();
             }
         }
